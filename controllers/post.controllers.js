437277226604c2b11f71318438ec93178f881f98
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-11-12 13:17:17 +0700

    update and delete own post

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -1,6 +1,6 @@
-// 1. Authenticated user can make a post (POST a post)
-// 2. Author can update post by post's id
-// 3. Author can delete post by post's id
+// 1. Authenticated user can make a post (POST a post) ✅
+// 2. Author can update post by post's id ✅
+// 3. Author can delete post by post's id ✅
 // 4. Friend can see list of friend's post
 
 const bcrypt = require("bcryptjs/dist/bcrypt");
@@ -15,6 +15,41 @@ postController.createPost = catchAsync(async (req, res, next) => {
   const { content, image } = req.body;
   let post = await Post.create({ author: currentUserId, content, image });
   return sendResponse(res, 200, true, post, null, "Create post success");
+});
+
+postController.updatePost = catchAsync(async (req, res, next) => {
+  const { currentUserId } = req;
+  const { postId } = req.params;
+  let post = await Post.findOne({ _id: postId, isDeleted: false });
+  if (!post) {
+    throw new AppError(404, "No post found", "No post found");
+  }
+  if (!post.author.equals(currentUserId)) {
+    throw new AppError(404, "Unauthorized", "Unauthorized");
+  }
+  const allows = ["content", "image"];
+  allows.forEach((field) => {
+    if (req.body[field] !== undefined) {
+      post[field] = req.body[field];
+    }
+  });
+  await post.save();
+  return sendResponse(res, 200, true, post, null, "Post updated");
+});
+
+postController.deletePost = catchAsync(async (req, res, next) => {
+  const { currentUserId } = req;
+  const { postId } = req.params;
+  let post = await Post.findOne({ _id: postId, isDeleted: false });
+  if (!post) {
+    throw new AppError(404, "No post found", "No post found");
+  }
+  if (!post.author.equals(currentUserId)) {
+    throw new AppError(404, "Unauthorized", "Unauthorized");
+  }
+  post.isDeleted = true;
+  await post.save();
+  return sendResponse(res, 200, true, {}, null, "Post deleted");
 });
 
 module.exports = postController;
--- routes/posts.api.js
+++ routes/posts.api.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { createPost } = require("../controllers/post.controllers");
+const {
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/post.controllers");
 
 const { loginRequired } = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/create", loginRequired, createPost);
+router.put("/:postId", loginRequired, updatePost);
+router.delete("/:postId", loginRequired, deletePost);
 
 module.exports = router;
 
