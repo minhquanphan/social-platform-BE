84476e82a1689addaff7f9519e14bae47f09727d
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-11-15 16:20:51 +0700

    updatee

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,18 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const commentSchema = Schema(
-//   {
-//     author: { type: Schema.Types.ObjectId, required: true, ref: "Users" },
-//     content: { type: String, required: true },
-//     image: { type: String, required: true },
-//     isDeleted: { type: Boolean, default: false },
-//   },
-//   {
-//     timestamps: true, //CreatedAt & UpdatedAt
-//   }
-// );
+const commentSchema = Schema(
+  {
+    author: { type: Schema.Types.ObjectId, required: true, ref: "Users" },
+    post: { type: Schema.Types.ObjectId, required: true, ref: "Posts" },
+    content: { type: String, required: true },
+    image: { type: String, required: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true, //CreatedAt & UpdatedAt
+  }
+);
 
-// const Comment = mongoose.model("Comments", commentSchema);
-// module.exports = Comment;
+const Comment = mongoose.model("Comments", commentSchema);
+module.exports = Comment;
