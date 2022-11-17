a2c13c98ce619216cb3962336c8e29e9e837a38c
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-11-17 21:43:13 +0700

    reaction flow done

--- controllers/reaction.controllers.js
+++ controllers/reaction.controllers.js
@@ -1,12 +1,47 @@
 // 5. Friend can make a reaction (like, dislike) to each other post ✅
 // 6. Friend can make a reaction (like, dislike) to each other comment ✅
 
+const mongoose = require("mongoose");
 const { catchAsync, sendResponse, AppError } = require("../helpers/utils");
+const Reaction = require("../models/Reaction");
 
 const reactionController = {};
 
-reactionController.reactionPosts = catchAsync(async (req, res, next) => {});
+reactionController.createReactions = catchAsync(async (req, res, next) => {
+  const { currentUserId } = req;
+  const { targetType, targetId, emoj } = req.body;
+  const collectionName = mongoose.model(targetType);
+  let target = await collectionName.findOne({ _id: targetId });
+  if (!target) {
+    throw new AppError("404", "Target not found", "Create reactions error");
+  }
 
-reactionController.reactionComments = catchAsync(async (req, res, next) => {});
+  let reaction = await Reaction.findOne({
+    targetType,
+    targetId,
+    author: currentUserId,
+  });
+
+  let message = "";
+  if (!reaction) {
+    await Reaction.create({
+      targetType,
+      targetId,
+      author: currentUserId,
+      emoj,
+    });
+  } else if (reaction.emoj === emoj) {
+    await reaction.delete();
+    message = "delete reaction";
+  } else {
+    reaction.emoj = emoj;
+    await reaction.save();
+    message = "change reaction emoji";
+  }
+
+  const reactions = await Reaction.find({ targetType, targetId });
+
+  sendResponse(res, 200, true, reactions, null, message);
+});
 
 module.exports = reactionController;
--- routes/reactions.api.js
+++ routes/reactions.api.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const { body } = require("express-validator");
+const { createReactions } = require("../controllers/reaction.controllers");
 
 const { loginRequired } = require("../middlewares/authentication");
 const { validate, checkObjectId } = require("../middlewares/validator");
 const router = express.Router();
+
+router.post(
+  "/create",
+  loginRequired,
+  validate([
+    body("targetId").exists().isString().custom(checkObjectId),
+    body("targetType").exists().isString(),
+  ]),
+  createReactions
+);
 
 module.exports = router;
